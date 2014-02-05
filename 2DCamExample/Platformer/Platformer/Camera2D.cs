e7d6725387b2533442fd5f034124276445448e35
Adam Beecham <apbeecham91@gmail.com>
2014-02-05 17:29:20 +0000

    Complete source and Executable

---
+++ 2DCamExample/Platformer/Platformer/Camera2D.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Platformer
+{
+    /// <summary>
+    /// A simple camera for 2D xna games.
+    /// </summary>
+    public class Camera2D
+    {
+        protected float zoom;
+        protected float rotation;
+
+        protected Matrix transform; 
+        protected Vector2 position;
+        
+        protected int viewportWidth;
+        protected int viewportHeight;
+
+        // the cameras zoom property. Higher
+        // values will make sprites appear larger
+        public float Zoom
+        {
+            get { return zoom; }
+            set 
+            { 
+                zoom = value; 
+                // we want to ensure that the zoom
+                // doesn't get too small with a simple
+                // check. If we allow the zoom to be zero
+                // the screen will appear blank. If we allow
+                // it to be negative, sprites will be drawn
+                // upside down!
+                if (zoom < 0.1f) zoom = 0.1f; 
+            }
+        }
+
+        // the cameras rotation property. For
+        // most simple games this can be ignored,
+        // but could be useful for example, in a top
+        // down shooter game.
+        public float Rotation
+        {
+            get { return rotation; }
+            set { rotation = value; }
+        }
+
+        // the cameras position in the game.
+        // The camera will be centered on this
+        // point.
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        /// <summary>
+        /// the camera constructor, sets up the camera
+        /// with some default values.
+        /// </summary>
+        /// <param name="initialPos">the cameras starting position in the game</param>
+        public Camera2D(Vector2 initialPos)
+        {
+            zoom = 1f;
+            rotation = 0.0f;
+            position = initialPos;
+        }
+
+        /// <summary>
+        /// moves the camera a specified amount.
+        /// </summary>
+        /// <param name="amount">the vector to add to the current position.</param>
+        public void Move(Vector2 amount)
+        {
+            position += amount;
+        }
+        
+        /// <summary>
+        /// gets the cameras transformation matrix.
+        /// </summary>
+        /// <param name="graphicsDevice">the graphics device.</param>
+        /// <returns>the transformation matrix</returns>
+        public Matrix GetTransformation(GraphicsDevice graphicsDevice)
+        {
+            viewportWidth = graphicsDevice.Viewport.Width;
+            viewportHeight = graphicsDevice.Viewport.Height;
+
+            // we create a transformation matrix using the various properties of
+            // of the camera. This matrix positions, rotates and scales everything in
+            // the game world respective to the cameras properties, which gives us the illusion
+            // that we have a camera that can move around, rotate and zoom in on our game world
+            transform = Matrix.CreateTranslation(new Vector3(-position.X, -position.Y, 0)) *
+                        Matrix.CreateRotationZ(Rotation) *
+                        Matrix.CreateScale(new Vector3(Zoom, Zoom, 1)) *
+                        Matrix.CreateTranslation(new Vector3(viewportWidth * 0.5f, viewportHeight * 0.5f, 0));
+            return transform;
+        }
+
+        /// <summary>
+        /// Inverts the cameras transformation matrix.
+        /// </summary>
+        /// <returns>the inverse transformation matrix</returns>
+        public Matrix InverseTransform()
+        {
+            //this is useful for when we want to negate
+            //the cameras transformation matrix. If we
+            //have a game that requires mouse input as well 
+            //as a camera, then we would need this method 
+            //to ensure that the mouses position is correct.
+            //You would call this method as follows:
+            //Vector2 mousePos = new Vector2(mousestate.X, mousestate.Y);
+            //mousePos = Vector2.Transform(mousePos, ContactGame.camera.GetInverse()));
+            //which would ensure the mouse position in the game is correct.
+            
+            Matrix inverse = Matrix.Invert(transform);
+            return inverse;
+        }
+    }
+}
+
---
+++ 2DCamExample/Platformer/Platformer/PlatformerGame.cs
@@ -0,0 +1,307 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// PlatformerGame.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input.Touch;
+
+
+namespace Platformer
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class PlatformerGame : Microsoft.Xna.Framework.Game
+    {
+        // Resources for drawing.
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+
+        // Global content.
+        private SpriteFont hudFont;
+
+        //our 2D Camera
+        private Camera2D camera;
+
+        private Texture2D winOverlay;
+        private Texture2D loseOverlay;
+        private Texture2D diedOverlay;
+
+        // Meta-level game state.
+        private int levelIndex = -1;
+        private Level level;
+        private bool wasContinuePressed;
+
+        // When the time remaining is less than the warning time, it blinks on the hud
+        private static readonly TimeSpan WarningTime = TimeSpan.FromSeconds(30);
+
+        // We store our input states so that we only poll once per frame, 
+        // then we use the same input state wherever needed
+        private GamePadState gamePadState;
+        private KeyboardState keyboardState;
+        private TouchCollection touchState;
+        private AccelerometerState accelerometerState;
+        
+        // The number of levels in the Levels directory of our content. We assume that
+        // levels in our content are 0-based and that all numbers under this constant
+        // have a level file present. This allows us to not need to check for the file
+        // or handle exceptions, both of which can add unnecessary time to level loading.
+        private const int numberOfLevels = 2;
+
+        public PlatformerGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+#if WINDOWS_PHONE
+            graphics.IsFullScreen = true;
+            TargetElapsedTime = TimeSpan.FromTicks(333333);
+#endif
+
+            Accelerometer.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Load fonts
+            hudFont = Content.Load<SpriteFont>("Fonts/Hud");
+
+            // Load overlay textures
+            winOverlay = Content.Load<Texture2D>("Overlays/you_win");
+            loseOverlay = Content.Load<Texture2D>("Overlays/you_lose");
+            diedOverlay = Content.Load<Texture2D>("Overlays/you_died");
+
+            //Known issue that you get exceptions if you use Media PLayer while connected to your PC
+            //See http://social.msdn.microsoft.com/Forums/en/windowsphone7series/thread/c8a243d2-d360-46b1-96bd-62b1ef268c66
+            //Which means its impossible to test this from VS.
+            //So we have to catch the exception and throw it away
+            try
+            {
+                MediaPlayer.IsRepeating = true;
+                MediaPlayer.Play(Content.Load<Song>("Sounds/Music"));
+            }
+            catch { }
+
+            //loads the next level (setup the camera after this
+            // as this is where the players position is set)
+            LoadNextLevel();
+            
+            //We instantiate the camera by passing a Vector2 to
+            //the constructor, specifying the initial position.
+            //Seeing as we want to follow the player, we set
+            //the initial position to the players position.
+            camera = new Camera2D(level.Player.Position);
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Handle polling for our input and handling high-level input
+            HandleInput();
+
+            // update our level, passing down the GameTime along with all of our input states
+            level.Update(gameTime, keyboardState, gamePadState, touchState, 
+                         accelerometerState, Window.CurrentOrientation);
+            camera.Position = level.Player.Position;
+
+            base.Update(gameTime);
+        }
+
+        private void HandleInput()
+        {
+            // get all of our input states
+            keyboardState = Keyboard.GetState();
+            gamePadState = GamePad.GetState(PlayerIndex.One);
+            touchState = TouchPanel.GetState();
+            accelerometerState = Accelerometer.GetState();
+
+            // Exit the game when back is pressed.
+            if (gamePadState.Buttons.Back == ButtonState.Pressed)
+                Exit();
+
+            bool continuePressed =
+                keyboardState.IsKeyDown(Keys.Space) ||
+                gamePadState.IsButtonDown(Buttons.A) ||
+                touchState.AnyTouch();
+
+            // Perform the appropriate action to advance the game and
+            // to get the player back to playing.
+            if (!wasContinuePressed && continuePressed)
+            {
+                if (!level.Player.IsAlive)
+                {
+                    level.StartNewLife();
+                }
+                else if (level.TimeRemaining == TimeSpan.Zero)
+                {
+                    if (level.ReachedExit)
+                        LoadNextLevel();
+                    else
+                        ReloadCurrentLevel();
+                }
+            }
+
+            wasContinuePressed = continuePressed;
+        }
+
+        private void LoadNextLevel()
+        {
+            // move to the next level
+            levelIndex = (levelIndex + 1) % numberOfLevels;
+
+            // Unloads the content for the current level before loading the next one.
+            if (level != null)
+                level.Dispose();
+
+            // Load the level.
+            string levelPath = string.Format("Content/Levels/{0}.txt", levelIndex);
+            using (Stream fileStream = TitleContainer.OpenStream(levelPath))
+                level = new Level(Services, fileStream, levelIndex);
+        }
+
+        private void ReloadCurrentLevel()
+        {
+            --levelIndex;
+            LoadNextLevel();
+        }
+
+        /// <summary>
+        /// Draws the game from background to foreground.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //we want the background to remain static, so
+            //we draw it in a seperate spritebatch call
+            //to avoid it being moved by our camera.
+            //Try uncommenting the level.DrawBg(spriteBatch) call
+            //between the spritebatch begin and end calls that specify
+            //the camera transformation to see why this is important
+            //(comment out this call first).
+            spriteBatch.Begin();
+            level.DrawBg(spriteBatch);
+            spriteBatch.End();
+
+            //This is the all important spriteBatch.Begin()
+            //call that takes our transformation matrix as an argument.
+            //we pass the GraphicsDevice to the cameras GetTransformation
+            //method which gives us access to the viewport boundaries
+            //which we use in calculating the transformation matrix.
+            //essentially, this matrix positions all the objects drawn to
+            //the screen relative to the position of the camera, which gives
+            //us the illusion that we are following the player.
+            spriteBatch.Begin(SpriteSortMode.Immediate,
+                           BlendState.NonPremultiplied,
+                           null,
+                           null,
+                           null,
+                           null,
+                           camera.GetTransformation(GraphicsDevice));
+            //level.DrawBg(spriteBatch);
+            level.Draw(gameTime, spriteBatch);
+
+            DrawHud();
+
+            spriteBatch.End();
+
+            
+
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawHud()
+        {
+            Rectangle titleSafeArea = GraphicsDevice.Viewport.TitleSafeArea;
+
+            //An important point to note here is the camera's GetInverse method. this
+            //method returns the inverse matrix of the camera's transform matrix, which we can
+            //use to ensure that our GUI elements, stay with the camera when we follow the player
+            //and not zoom off in to the distance. We could also draw these elements in a seperate
+            //spriteBatch call (as with the background) instead of using the inverse matrix to
+            //position these elements. The call to Vector2.Transform is required to reposition
+            //the hudLocation and center position to the cameras position, using the inverse matrix.
+            //NOTE: In your own games DO NOT use the inverse transform to position hud elements. call
+            //them in a seperate spriteBatch instead, like the background image in this game. this is simply
+            //a demonstration of how the method works.
+            Vector2 hudLocation = Vector2.Transform(new Vector2(titleSafeArea.X, titleSafeArea.Y),camera.InverseTransform());
+            Vector2 center = Vector2.Transform(new Vector2(titleSafeArea.X + titleSafeArea.Width / 2.0f,
+                                         titleSafeArea.Y + titleSafeArea.Height / 2.0f),camera.InverseTransform());
+
+            // Draw time remaining. Uses modulo division to cause blinking when the
+            // player is running out of time.
+            string timeString = "TIME: " + level.TimeRemaining.Minutes.ToString("00") + ":" + level.TimeRemaining.Seconds.ToString("00");
+            Color timeColor;
+            if (level.TimeRemaining > WarningTime ||
+                level.ReachedExit ||
+                (int)level.TimeRemaining.TotalSeconds % 2 == 0)
+            {
+                timeColor = Color.Yellow;
+            }
+            else
+            {
+                timeColor = Color.Red;
+            }
+            DrawShadowedString(hudFont, timeString, hudLocation, timeColor);
+
+            // Draw score
+            float timeHeight = hudFont.MeasureString(timeString).Y;
+            DrawShadowedString(hudFont, "SCORE: " + level.Score.ToString(), hudLocation + new Vector2(0.0f, timeHeight * 1.2f), Color.Yellow);
+           
+            // Determine the status overlay message to show.
+            Texture2D status = null;
+            if (level.TimeRemaining == TimeSpan.Zero)
+            {
+                if (level.ReachedExit)
+                {
+                    status = winOverlay;
+                }
+                else
+                {
+                    status = loseOverlay;
+                }
+            }
+            else if (!level.Player.IsAlive)
+            {
+                status = diedOverlay;
+            }
+
+            if (status != null)
+            {
+                // Draw status message.
+                Vector2 statusSize = new Vector2(status.Width, status.Height);
+                spriteBatch.Draw(status, center - statusSize / 2, Color.White);
+            }
+        }
+
+        private void DrawShadowedString(SpriteFont font, string value, Vector2 position, Color color)
+        {
+            spriteBatch.DrawString(font, value, position + new Vector2(1.0f, 1.0f), Color.Black);
+            spriteBatch.DrawString(font, value, position, color);
+        }
+    }
+}
